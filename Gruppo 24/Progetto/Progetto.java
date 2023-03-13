2f995b7e7f876eda8e97da7adb8bfab6aebbee02
UniMassy02 <m.podda@studenti.unibg.it>
2023-03-13 16:11:31 +0100

    funge

--- Gruppo 24/Progetto/Progetto.java
+++ Gruppo 24/Progetto/Progetto.java
@@ -4,6 +4,7 @@ public class Progetto {
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo!");
 		System.out.println("funziona?");
+		System.out.println("Arrivato");
 		
 	}
 
