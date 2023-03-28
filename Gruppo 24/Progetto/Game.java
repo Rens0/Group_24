6e457df5ba0fd534c31a1a0d4f9255741ec10bcd
Annalisa <Cosimo@LAPTOP-UOI7DPDC>
2023-03-28 19:22:00 +0200

    bozza giocatore

---
+++ Gruppo 24/Progetto/Game.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Game {
+
+	public static void main(String[] args) {
+		int number_of_players;
+		String name; 
+		do { 
+		System.out.println("Insert number of players: "); 
+		Scanner scanner = new Scanner (System.in); 
+		number_of_players = scanner.nextInt();
+		if (number_of_players<2 || number_of_players>4) {
+			System.out.println("Please insert from 2 to 4 players "); 
+		}
+		}while (number_of_players<2 || number_of_players>4); 
+		
+		Player [] p=new Player[number_of_players];
+		
+			for (int i=0; i<number_of_players; i++) {
+				System.out.println("Insert your name: "); 
+				Scanner in = new Scanner (System.in); 
+				name=in.nextLine(); 
+				p[i]=new Player(name);
+			}
+		  
+				
+		
+		p[0].print();
+		p[1].print();
+	}
+	
+
+}
---
+++ Gruppo 24/Progetto/Player.java
@@ -0,0 +1,30 @@
+
+
+public class Player {
+
+	private String name;
+	private int ID; 
+	private static int ID_Number=0; 
+	public Player() {}
+	public Player (String name) {
+		this.name=name;
+		//ID_Number++;
+		this.ID=ID_Number();  
+		//this.ID=ID_Number; 
+	}
+	
+	private int ID_Number () {
+		ID_Number++;
+		ID=ID_Number;
+		return ID; 
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////
+	public void print() {
+		System.out.println("name: "+name);
+		System.out.println(name+"'s ID is: "+ID);
+	}
+	
+	
+
+}
