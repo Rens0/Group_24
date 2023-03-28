6e22736972a14a432d776e7953f44d39bc70038d
hp <hp@DESKTOP-SQA01K9>
2023-03-28 21:56:41 +0200

    mah

--- Gruppo 24/Progetto/Progetto.java
+++ Gruppo 24/Progetto/Progetto.java
@@ -1,12 +1,17 @@
+import prog.io.ConsoleInputManager;
+import prog.io.ConsoleOutputManager;
 
 public class Progetto {
 
 	public static void main(String[] args) {
+		ConsoleInputManager in = new ConsoleInputManager();
+		ConsoleOutputManager out = new ConsoleOutputManager();
 		System.out.println("Ciao mondo!");
 		System.out.println("funziona?");
 		System.out.println("Arrivato");
 		System.out.println("si");
 		System.out.println("Push dopo il cambio di nome");
+		System.out.println("Arrivato");
 		System.out.println("Arrivato");
 	}
 
