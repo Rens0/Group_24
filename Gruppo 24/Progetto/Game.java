81980d22d06a6b736f74b04c1862c2e1426a177e
UniMassy02 <m.podda@studenti.unibg.it>
2023-03-29 15:56:51 +0200

    migliorato ancora

--- Gruppo 24/Progetto/Game.java
+++ Gruppo 24/Progetto/Game.java
@@ -3,35 +3,46 @@ import java.util.Scanner;
 public class Game {
 
 	public static void main(String[] args) {
-		int number=0;
+		int id=0;
 		String name; 
 		boolean verifica=true;
-		
+		int scelta=0;
+		int number=0;
 		System.out.println("Welcome to the game,\nnumber max of player: 4");
 		
 		do { 
 			
 			System.out.println("Insert your name: "); 
-			
+			number++;
 			
 			Player [] p=new Player[4];
 			Scanner scannerplayer = new Scanner (System.in); 
 			name=scannerplayer.nextLine(); 
-			p[number]=new Player(name,number);
-			p[number].print();
-			number++;
+			p[id]=new Player(name,id);
+			p[id].print();
+			
 		
 			
 		
 		if (number>=2) {
-			
-			if(number==4)
+			System.out.println("Premi 1 per terminare l'inserimento dei giocatori,"
+					+ "\naltrimenti premi un altro numero per continuare l'inserimento"+ "");
+			Scanner scannerint = new Scanner(System.in);
+			scelta=scannerint.nextInt();
+			if(number==3)
 			{
 				break;
+			}else
+			{
+				if(scelta==1) 
+				
+			{
+			verifica=false;
+			}
 			}
 			
 		}
-
+		id++;
 		
 		}while (verifica); 
 		
