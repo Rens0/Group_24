e28cf4b72fce86ababea9c954a60e40cd4bdd051
Annalisa <Cosimo@LAPTOP-UOI7DPDC>
2023-03-30 11:46:41 +0200

    Bozza Player

--- Gruppo 24/Progetto/Player.java
+++ Gruppo 24/Progetto/Player.java
@@ -28,3 +28,24 @@ public class Player {
 	
 
 }
+ /*public class Player {
+
+	private String name;
+	private int ID; 
+	
+	public Player (String name, int Number) {
+		this.name=name;
+		this.ID=Number;  
+		
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////
+	public void print() {
+		System.out.println("name: "+name);
+		System.out.println(name+"'s ID is: "+ID);
+	}
+	
+	
+
+}*/
+
