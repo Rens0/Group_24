994f0f7d7137d826e62faa79ea327f0677367a2b
Annalisa <Cosimo@LAPTOP-UOI7DPDC>
2023-03-30 12:59:56 +0200

    Player

--- Gruppo 24/Progetto/Game.java
+++ Gruppo 24/Progetto/Game.java
@@ -5,9 +5,9 @@ public class Game {
 	public static void main(String[] args) {
 		int number_of_players;
 		String name; 
+		Scanner scanner = new Scanner (System.in); 
 		do { 
 		System.out.println("Insert number of players: "); 
-		Scanner scanner = new Scanner (System.in); 
 		number_of_players = scanner.nextInt();
 		if (number_of_players<2 || number_of_players>4) {
 			System.out.println("Please insert from 2 to 4 players "); 
--- Gruppo 24/Progetto/Player.java
+++ Gruppo 24/Progetto/Player.java
@@ -3,7 +3,7 @@ public class Player {
 	private String name;
 	private int ID; 
 	private static int ID_Number=0; 
-	public Player() {}
+	
 	public Player (String name) {
 		this.name=name;
 		//ID_Number++;
