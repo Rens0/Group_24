37a9a77a3cb55c7ebd133787254be9e076d34c3c
UniMassy02 <m.podda@studenti.unibg.it>
2023-04-26 16:42:44 +0200

    prima prova interfaccia

---
+++ src/view/frame.java
@@ -0,0 +1,19 @@
+package view;
+import javax.swing.*;
+
+public class frame {
+    public static void main(String[] args) {
+        JFrame  frame = new JFrame("My BookShelfie");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // per terminere l'esecuzione quando si preme la X
+        frame.setSize(800,500); //dimensione dello schermo
+        frame.setLocationRelativeTo(null); //setta l'apertura della finestra nel centro dello schermo
+        frame.setVisible(true); // setta visibile la finestra
+
+
+
+
+
+
+    }
+}
