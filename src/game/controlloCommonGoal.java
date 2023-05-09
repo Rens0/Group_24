574ebf99755dd18b3a8f786689f9d66a0e04547b
UniMassy02 <m.podda@studenti.unibg.it>
2023-05-09 18:50:49 +0200

    ottimizzazione controlloCommonGoal.java e implementazione common goal 7,6,3, coprogrammazione @gserra3

--- src/game/controlloCommonGoal.java
+++ src/game/controlloCommonGoal.java
@@ -1,5 +1,7 @@
 package game;
 
+import card.CardContainer;
+
 public class controlloCommonGoal {
     private static int COLONNE = 5;
     private static int RIGHE = 6;
@@ -13,14 +15,22 @@ public class controlloCommonGoal {
     }
     public boolean controlloCommonGoal3(Libreria libreria)
     {
-        if(libreria.celle.get(0).get(0).tile.type==libreria.celle.get(0).get(4).tile.type&&
-                libreria.celle.get(0).get(0).tile.type==libreria.celle.get(5).get(4).tile.type&&
-                libreria.celle.get(0).get(0).tile.type==libreria.celle.get(5).get(0).tile.type)
+
+        if(ritornoTipo(0,0,libreria)==ritornoTipo(0,libreria.celle.get(0).size()-1,libreria)&&
+                ritornoTipo(0,0,libreria)==ritornoTipo(libreria.celle.size()-1,libreria.celle.get(0).size()-1,libreria)&&
+                ritornoTipo(0,0,libreria)==ritornoTipo(libreria.celle.size()-1, 0,libreria)
+        )
         {
           return true;
         }
 
        return false;
+    }
+    public String ritornoTipo(int riga, int colonna, Libreria libreria)
+    {
+        String tipo;
+        tipo= libreria.celle.get(riga).get(colonna).tile.type;
+        return tipo;
     }
     public boolean controlloCommonGoal4(Libreria libreria)
     {
@@ -30,8 +40,8 @@ public class controlloCommonGoal {
     {
         return true;
     }
-    public boolean controlloCommonGoal6(Libreria libreria) {
-        int cont1 = 0;
+    public boolean controlloCommonGoal6(Libreria libreria, CardContainer cardContainer) {
+     /*   int cont1 = 0;
         int cont2 = 0;
         int cont3 = 0;
         int cont4 = 0;
@@ -76,16 +86,37 @@ public class controlloCommonGoal {
                 }
             }
 
+        }*/
+        //versione ottimizzata
+
+        for(int c=0;c<cardContainer.list.size();c++)
+        {
+            int contacarte=0;
+            for(int i=0; i<libreria.celle.size();i++)
+            {
+                for(int j=0; j<libreria.celle.get(i).size();j++)
+                {
+                    if(ritornoTipo(i,j,libreria)==cardContainer.list.get(c).type)
+                    {
+                        contacarte++;
+                        if(contacarte>=8)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
         }
         return false;
     }
 
     public boolean controlloCommonGoal7(Libreria libreria)
     {
-        // da testare
+        /*  ottimizzato con nuova funzione
         boolean foundMainDiagonalValue = true;
         boolean foundSecondaryDiagonalValue = true;
         for (int i = 1; i < 5; i++) {
+
             if (libreria.celle.get(i).get(i).tile.type != libreria.celle.get(0).get(0).tile.type) {
                 foundMainDiagonalValue = false;
             }
@@ -93,8 +124,44 @@ public class controlloCommonGoal {
                 foundSecondaryDiagonalValue = false;
             }
         }
-        return foundMainDiagonalValue || foundSecondaryDiagonalValue;
+        return foundMainDiagonalValue || foundSecondaryDiagonalValue;*/
+
+        for(int i=0; i<libreria.celle.size();i++)
+        {
+            int contDiagoPrinc=0;
+            int contDiagoSec=0;
+            if((libreria.celle.size()-i)>=libreria.celle.get(0).size())
+            {
+                for(int j=0;j<libreria.celle.get(i).size();j++)
+                {
+                    if (ritornoTipo(j+i,j,libreria) == ritornoTipo(0,0,libreria))
+                    {
+                        contDiagoPrinc++;
+                        if(contDiagoPrinc>=libreria.celle.get(0).size())
+                        {
+                            return true;
+                        }
+                    }
+
+                    if (ritornoTipo(0,libreria.celle.get(0).size(),libreria)==
+                            ritornoTipo(j+i,libreria.celle.get(0).size()-j-1,libreria))
+                    {
+                        contDiagoSec++;
+                        if(contDiagoSec>=libreria.celle.get(0).size())
+                        {
+                                return true;
+                        }
+                    }
+
+
+                }
+            }
+
+
+        }
+        return false;
     }
+
     public boolean controlloCommonGoal8(Libreria libreria)
     {
         return true;
