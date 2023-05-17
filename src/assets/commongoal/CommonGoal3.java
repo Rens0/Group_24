335f2dc0e384e4d955df005818b4d316b23efa97
UniMassy02 <m.podda@studenti.unibg.it>
2023-05-17 17:06:04 +0200

    aggiornata la classe frame con implementato la creazione di una nuova finestra dopo aver premuto il pulsante per sartare il gioco

--- src/assets/commongoal/CommonGoal3.java
+++ src/assets/commongoal/CommonGoal3.java
@@ -1,4 +1,10 @@
 package assets.commongoal;
 
+import assets.component.Libreria;
+
 public class CommonGoal3 extends ControlloCommonGoal{
+    public boolean controlloCommonGoal3(Libreria libreria)
+    {
+        return true;
+    }
 }
