8bd3d66813760dfbfba93f1af59ea312fd257e26
Annalisa <a.sergi2@studenti.unibg.it>
2023-06-01 20:20:58 +0200

    aggiunta commento per indicare il main

--- src/Game.java
+++ src/Game.java
@@ -13,7 +13,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * Inizializza l'applicazione
+ *
+ */
 public class Game {
 
     public static void main(String[] args) {
